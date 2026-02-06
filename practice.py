1be4c55e7830adc1a4e961650652628f5e060d16
William Lewison <grifflew@unc.edu>
2026-02-05 19:27:47 -0500

    ex02_charldle

--- practice.py
+++ practice.py
@@ -5,3 +5,28 @@ def celsius_to_fahrenheit(degrees: int) -> float:
 def perimeter(length: float, width: float) -> float:
     """calculate the perimeter of a rectangle"""
     return length * 2 + width * 2
+
+
+def total_cost(price: int, tax_rate: float) -> float:
+    return price + (price * tax_rate)
+
+
+print(total_cost(price=100, tax_rate=0.07))
+
+
+def fuel_needed(distance: int, mpg: int) -> float:
+    return distance / mpg
+
+
+def total_fuel_cost(distance: int, mpg: int, price_per_gallon: int) -> float:
+    return fuel_needed(distance=distance, mpg=mpg) * price_per_gallon
+
+
+print(total_fuel_cost(distance=300, mpg=25, price_per_gallon=4))
+
+
+def pos_or_neg(num: int) -> str:
+    if num > 0:
+        return "Positive"
+    else:
+        return "Negative"
